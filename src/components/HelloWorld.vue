0707072c4048c3f08b587299bb9ef29313720c75
Prometheus <1520406381@qq.com>
2024-07-21 10:22:27 +0800

    引入draggable

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,35 @@
 <template>
   <div class="hello">
-
+    <draggable :list="list" ghost-class="ghost" :force-fallback="true" chosen-class="chosenClass" animation="300"
+      @start="onStart" @end="onEnd">
+      <template #item="{ element }">
+        <div class="item">
+          {{ element.name }}
+        </div>
+      </template>
+    </draggable>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { ref, reactive } from "vue";
 import draggable from "vuedraggable";
+const list = ref([
+  { name: "www.itxst.com", id: 0 },
+  { name: "www.baidu.com", id: 1 },
+  { name: "www.google.com", id: 2 },
+])
+const onStart = () => {
+  console.log("start")
+}
 
+const onEnd = () => {
+  console.log("end")
+}
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.item {
+  cursor: move;
+}
+</style>
