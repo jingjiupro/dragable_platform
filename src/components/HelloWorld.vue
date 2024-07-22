d18ae254bf758682d0900a46d77b40571057337f
Prometheus <1520406381@qq.com>
2024-07-22 10:29:34 +0800

    双向拖拽

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,23 @@
 <template>
   <div class="hello">
-    <draggable :list="list" ghost-class="ghost" :force-fallback="true" chosen-class="chosenClass" animation="300"
-      @start="onStart" @end="onEnd">
+    <el-divider content-position="left">已选择</el-divider>
+    <draggable v-model="selectedApp" group="app" item-key="quickEntryName" class="app-list" animation="500">
       <template #item="{ element }">
-        <div class="item">
-          {{ element.name }}
+        <div class="app-item dialog-item">
+          <div class="item-div already-item-div">
+            <span class="icon-name">{{ element.quickEntryName }}</span>
+          </div>
+        </div>
+      </template>
+    </draggable>
+    <el-divider content-position="left">全部</el-divider>
+    <draggable v-model="allApp" group="app" item-key="quickEntryName" class="app-list" animation="500" @start="onStart"
+      @end="onEnd">
+      <template #item="{ element }">
+        <div class="app-item dialog-item">
+          <div class="item-div already-item-div">
+            <span class="icon-name">{{ element.quickEntryName }}</span>
+          </div>
         </div>
       </template>
     </draggable>
@@ -12,13 +25,28 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, reactive } from "vue";
+import { ref } from "vue";
 import draggable from "vuedraggable";
-const list = ref([
-  { name: "www.itxst.com", id: 0 },
-  { name: "www.baidu.com", id: 1 },
-  { name: "www.google.com", id: 2 },
+const selectedApp = ref([
+  { quickEntryName: "菜单1" },
+  { quickEntryName: "菜单2" },
+  { quickEntryName: "菜单3" },
+  { quickEntryName: "菜单4" },
+  { quickEntryName: "菜单5" },
 ])
+
+const allApp = ref([
+  { quickEntryName: "菜单6" },
+  { quickEntryName: "菜单7" },
+  { quickEntryName: "菜单8" },
+  { quickEntryName: "菜单9" },
+  { quickEntryName: "菜单10" },
+  { quickEntryName: "菜单11" },
+  { quickEntryName: "菜单12" },
+  { quickEntryName: "菜单13" },
+  { quickEntryName: "菜单14" },
+])
+
 const onStart = () => {
   console.log("start")
 }
@@ -29,7 +57,58 @@ const onEnd = () => {
 </script>
 
 <style scoped lang="scss">
-.item {
-  cursor: move;
+.hello {
+  width: 50%;
+  margin: 0 auto;
+}
+
+.app-list {
+  display: flex;
+  flex-wrap: wrap;
+
+  .app-item {
+    margin-bottom: 15px;
+
+    .item-div {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      cursor: cursor;
+      width: 98px;
+      margin: 10px;
+      padding: 18px 0;
+      border: 1px solid gray;
+      border-radius: 5px;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+
+      &:hover {
+        background: #ffffff;
+        box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.12);
+        border-radius: 4px 4px 4px 4px;
+      }
+
+      .icon-name {
+        font-size: 14px;
+        color: #2d2e3b;
+        margin-top: 6px;
+      }
+    }
+
+    .already-item-div {
+      position: relative;
+
+      .already-remove {
+        color: #ff3434;
+        position: absolute;
+        right: 14px;
+        top: 0px;
+        font-size: 20px;
+      }
+    }
+  }
 }
 </style>
